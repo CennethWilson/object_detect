0dd8659ad83b4d9ed5157e52e5848a459ad11162
Cenneth <Cenneth@LAPTOP-KTIGEC6D>
2025-07-13 13:55:52 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+from ultralytics import YOLO
+
+import numpy as np
+import cv2
+
+app = Flask(__name__)
+model = YOLO("yolov11n.pt")
+
+@app.route("/detect", methods=["POST"])
+def detect():
+    img_data = request.files['image'].read()
+    nparr = np.frombuffer(img_data, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    results = model.predict(image)
+
+    df = results[0].pandas().xyxy[0]
+    return jsonify(df.to_dict(orient="records"))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
